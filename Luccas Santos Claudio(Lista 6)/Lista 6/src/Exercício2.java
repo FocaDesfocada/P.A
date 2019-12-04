284700efbb3c2a03a67ed5a0d791c06d886d6a3c
Vitor Carmo <56807155+Vitor-Carmo@users.noreply.github.com>
2019-12-03 23:44:23 -0300

    Update Exercício2.java
    A definição de número primo é que  ele é divisível por 1 e por ele mesmo (A%1==0 && A%A==0).
    
    Sendo assim, Percebemos que um número primo tem apenas o total de 2 números sendo divisíveis por ele (sendo esses 1 e ele mesmo).
    
    Logo, para determinar se um número é primo ou não, basta contar o tanto de vezes que ele é divisível pelos números de 1 até A (Sendo A o numero primo e A >= 2). 
    
    Então, fiz um laço de repetição "c" começando do 1 e indo até o numero A[ Na posição "I" ]. Dentro desse laço tem uma verificação com apenas um if. Se  A[ Na posição "I" ] for divisível por c (Sendo c variando de 1 até A[ I ]), então faça o vetor "TotalRestoPrimos" [ Na mesma posição do vetor A ] Receber TotalRestoPrimos+1 (Lembrando que fizemos o vetor TotalRestoPrimos receber 0 em todas posições dele).
    Fazendo isto, ele vai contar quantas vezes o A[ I ] é divisível pelo um número.
    
    Depois é só verificar se TotalRestoPrimos[ I ] é igual a 2. Se for, o número é primo e você soma 1 no b.  Logo após, você mostra para o usuário quantos números primos foram digitados mostrando para ele a variável b.

--- Luccas Santos Claudio(Lista 6)/Lista 6/src/Exercício2.java
+++ Luccas Santos Claudio(Lista 6)/Lista 6/src/Exercício2.java
@@ -1,30 +1,38 @@
 import java.util.Scanner;
-public class Exercício2 {
+public class ExercÃ­cio2 {
 
-	public static void main(String[] args) {
-		int a=2, b=0, i, A[];
-		A = new int[10];
-		Scanner in = new Scanner(System.in);
-		
-		
-		for(i=0; i<A.length; i++) {
-			System.out.println("Digite o "+(i+1)+"º valor");
-			A[i] = in.nextInt();
-			
-			if(A[i] % a == 0 && a < A[i]) {
-				
-				System.out.println("O número não é primo");
-				
-			}else {
-				
-				System.out.println("O número é primo");
-				b++;
-			}
-			
-		}
-		
-		System.out.println("O número de valores primos encontrados foi: "+b);
-		in.close();
-	}
+    public static void main(String[] args) {
+        int TotalRestoPrimos[], b=0, i, A[];
+        A = new int[10];
+        TotalRestoPrimos = new int[10];
+
+        Scanner in = new Scanner(System.in);
+
+
+        for(i=0; i<A.length; i++) {
+            System.out.println("Digite o "+(i+1)+"Âº valor");
+            A[i] = in.nextInt();
+            TotalRestoPrimos[i] = 0;
+
+            for(int c=1;c<=A[i];c++){
+
+                if(A[i]%c==0){
+                    TotalRestoPrimos[i]++;
+                }
+
+            }
+        }
+
+
+        for (i=0;i<A.length;i++) {
+            if (TotalRestoPrimos[i] == 2) {
+                b++;
+            }
+        }
+
+
+        System.out.println("O nÃºmero de valores primos encontrados foi: "+b);
+        in.close();
+    }
 
 }
